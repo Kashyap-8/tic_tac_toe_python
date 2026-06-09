644fd4f5c5957fcbf5d447a07599ef27c0914f52
Kash <Kashyapkhatri.8@gmail.com>
2026-06-08 17:37:14 -0700

    style: change to read more pythonic less c-like

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ class Gamestate:
         for row in range(3): 
             for col in range(3): 
                 print(' ' + self.board[row][col] + ' ', end='') #no new line after cell
-                if (col < 2): 
+                if col < 2: 
                     print('|', end='')
             print() #end of row
-            if(row < 2):
+            if row < 2:
                 print("---|---|---")
 
     def make_move(self, row, col):
-        if (row < 0 or row > 2 or col < 0 or col >2):
+        if  row < 0 or row > 2 or col < 0 or col >2:
             return False
-        if (self.board[row][col] != ' '): 
+        if  self.board[row][col] != ' ': 
             return False
         self.board[row][col] = self.current_player
         self.moves_made += 1 
@@ -30,39 +30,39 @@ class Gamestate:
     
     def check_winner(self):
         #check row (Note: unlike c python let's use use chain comparisions)
-        if(self.board[0][0] == self.board[0][1] == self.board[0][2] != ' '):
+        if self.board[0][0] == self.board[0][1] == self.board[0][2] != ' ':
             return self.board[0][0]
-        if(self.board[1][0] == self.board[1][1] == self.board[1][2] != ' '):
+        if self.board[1][0] == self.board[1][1] == self.board[1][2] != ' ':
             return self.board[1][0]
-        if(self.board[2][0] == self.board[2][1] == self.board[2][2] != ' '):
+        if self.board[2][0] == self.board[2][1] == self.board[2][2] != ' ':
             return self.board[2][0]
         
         #check columns
-        if(self.board[0][0] == self.board[1][0] == self.board[2][0] != ' '):
+        if self.board[0][0] == self.board[1][0] == self.board[2][0] != ' ':
             return self.board[0][0]
-        if(self.board[0][1] == self.board[1][1] == self.board[2][1] != ' '):
+        if self.board[0][1] == self.board[1][1] == self.board[2][1] != ' ':
             return self.board[0][1]
-        if(self.board[0][2] == self.board[1][2] == self.board[2][2] != ' '):
+        if self.board[0][2] == self.board[1][2] == self.board[2][2] != ' ':
             return self.board[0][2]
         
         #check diagonals 
-        if(self.board[0][0] == self.board[1][1] == self.board[2][2] != ' '):
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
             return self.board[0][0]
-        if(self.board[2][0] == self.board[1][1] == self.board[0][2] != ' '):
+        if self.board[2][0] == self.board[1][1] == self.board[0][2] != ' ':
             return self.board[2][0] 
         
-        if(self.moves_made == 9):
+        if self.moves_made == 9:
             return 'D'
         return ' '
     
     def switch_player(self): 
-        if (self.current_player == 'x'):
+        if  self.current_player == 'x':
             self.current_player = 'o'
         else:
             self.current_player = 'x'
 
     def game_loop(self):
-        while(True):
+        while True:
             self.print_board()
             try:
                 #the user input must be inside the try
@@ -76,12 +76,12 @@ class Gamestate:
                 print("Invalid input please enter two coordinates eg. 0 0")
                 continue
             #catches bad coordinate
-            if not self.make_move(row, col): 
+            if not self.make_move (row, col): 
                 print("Invalid move try again")
                 continue
 
             winner = self.check_winner()
-            if (winner) != ' ':
+            if  winner != ' ':
                 self.print_board()
                 if winner == 'D':
                     print("It's a draw")
@@ -109,7 +109,7 @@ while running:
 
     pygame.display.flip()
 
-    clock.tick(60) #limits fps to 60 Hz
+    clock.tick(60) #limits fps to 60 (60Hz)
 
 pygame.quit()
 
